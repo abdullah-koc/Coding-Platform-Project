c7a9b4f337fd20cca3a6438cf443fc5181e55c59
abdullah-koc <abdullah.koc@ug.bilkent.edu.tr>
2022-02-25 10:32:39 +0300

    Login was completed

---
+++ coding-frontend/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import LoginRegister from "./views/LoginRegister";
+import { makeStyles } from "@mui/styles";
+import clsx from "clsx";
+
+const useStyles = makeStyles({
+  root: {
+    height: "100vh",
+    width: "100vw",
+    backgroundColor: "#fafafa",
+  },
+  loginregister: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+const App = () => {
+  const classes = useStyles();
+  return (
+    <div className={clsx(classes.root, classes.loginregister)}>
+      <div>
+        <LoginRegister />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ coding-frontend/src/utils/Colors.js
@@ -0,0 +1,8 @@
+class Colors {
+  static primary_color = "#008773";
+  static secondary_color = "#00c8ac";
+  static dark_color = "#2a454e";
+  static secondary_dark_color = "#385a64";
+}
+
+export default Colors;
