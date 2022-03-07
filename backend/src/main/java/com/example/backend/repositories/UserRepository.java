952956a141db722d5e49e6e471367d351e181627
Arda Serim <arda.serim@ug.bilkent.edu.tr>
2022-03-07 18:01:55 +0300

    JDBC Library added

--- backend/src/main/java/com/example/backend/repositories/UserRepository.java
+++ backend/src/main/java/com/example/backend/repositories/UserRepository.java
@@ -2,14 +2,21 @@ package com.example.backend.repositories;
 
 import com.example.backend.entities.Person;
 import com.example.backend.entities.User;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends PersonRepository{
+public class UserRepository {
 
-    //@Query(value = "INSERT INTO persons (full_name, email, nickname, password, birth_date )
-    //VALUES (user.getFull_name(), user.getEmail(), user.getNickname(), user.getPassword(), user.getBirth_date())")
-    //User signUp(@Param("user") User user);
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public User signUp(User user) {
+        jdbcTemplate.update(
+                "INSERT INTO persons (full_name, email, nickname, password, birth_date ) VALUES (?, ?, ?, ?, ?)",
+                user.getFull_name(), user.getEmail(), user.getNickname(), user.getPassword(), user.getBirth_date());
+        return user;
+    }
 }
