3eeaffb9e22774672f43051fbf02fe715e1d6cbb
abdullah-koc <abdullah.koc@ug.bilkent.edu.tr>
2022-04-05 16:36:40 +0300

    commit

--- coding-frontend/src/components/UserStatus.js
+++ coding-frontend/src/components/UserStatus.js
@@ -2,9 +2,11 @@ import React from "react";
 import { TextField, InputAdornment, Grid, Button, colors } from "@mui/material";
 import Colors from "../utils/Colors";
 import Box from "@mui/material/Box";
+import NavbarUser from "./Navbars/NavbarUser";
 
 const UserStatus = () => {
-    const [successRate, setSuccessRate] = React.useState(80);
+  const [successRate, setSuccessRate] = React.useState(80);
+
   return (
     <Box
       sx={{
@@ -17,7 +19,7 @@ const UserStatus = () => {
     >
       <Grid container>
         <Grid item xs={6} style={{ marginTop: "6%", paddingLeft: "6%" }}>
-            <Grid container direction="column">
+          <Grid container direction="column">
             <Grid item xs={5} style={{ marginTop: "6%" }}>
               <Grid
                 item
@@ -29,23 +31,31 @@ const UserStatus = () => {
             <Grid item xs={7} style={{ marginTop: "18%" }}>
               <Grid
                 item
-                style={{ float: "left",
-                color: successRate < 40 ? Colors.hard_red_color : successRate >= 40 && successRate < 70 ? Colors.medium_yellow_color : successRate >= 70 ? Colors.easy_green_color : Colors.easy_green_color,
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                width: "80px",
-                height: "80px",
-                fontSize: "150%",
-                backgroundColor: Colors.dark_color,
-                borderRadius: "50%" ,
-                border: "3px solid"
-            }}
+                style={{
+                  float: "left",
+                  color:
+                    successRate < 40
+                      ? Colors.hard_red_color
+                      : successRate >= 40 && successRate < 70
+                      ? Colors.medium_yellow_color
+                      : successRate >= 70
+                      ? Colors.easy_green_color
+                      : Colors.easy_green_color,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "80px",
+                  height: "80px",
+                  fontSize: "150%",
+                  backgroundColor: Colors.dark_color,
+                  borderRadius: "50%",
+                  border: "3px solid",
+                }}
               >
-                  {successRate}%
+                {successRate}%
               </Grid>
             </Grid>
-            </Grid>
+          </Grid>
         </Grid>
         <Grid item xs={6}>
           <Grid container direction="column">
@@ -60,7 +70,7 @@ const UserStatus = () => {
                 item
                 style={{ float: "right", color: Colors.statistics_color }}
               >
-                12/100
+                46/50
               </Grid>
             </Grid>
             <Grid item xs={4} style={{ marginTop: "8%" }}>
@@ -74,7 +84,7 @@ const UserStatus = () => {
                 item
                 style={{ float: "right", color: Colors.statistics_color }}
               >
-                47/100
+                24/36
               </Grid>
             </Grid>
             <Grid item xs={4} style={{ marginTop: "8%" }}>
@@ -88,7 +98,7 @@ const UserStatus = () => {
                 item
                 style={{ float: "right", color: Colors.statistics_color }}
               >
-                27/100
+                12/30
               </Grid>
             </Grid>
           </Grid>
--- coding-frontend/src/views/LoginRegister/Register.js
+++ coding-frontend/src/views/LoginRegister/Register.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { TextField, InputAdornment, Grid, Button } from "@mui/material";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import VpnKeyIcon from "@mui/icons-material/VpnKey";
@@ -11,6 +11,22 @@ import Colors from "../../utils/Colors";
 
 const Register = () => {
   const [isEditor, setIsEditor] = useState(false);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [nickName, setNickName] = useState("");
+  const [password, setPassword] = useState("");
+  const [birthday, setBirthday] = useState("");
+
+  const [testData, setTestData] = useState();
+  useEffect(() => {
+    fetch(`${process.env.REACT_PHP_ENDPOINT}`, {
+      method: "GET",
+      action: "test",
+    })
+      .then((res) => res.json())
+      .then((data) => setTestData(data));
+  }, []);
+
   return (
     <Grid
       container
@@ -18,6 +34,7 @@ const Register = () => {
         marginLeft: "13%",
       }}
     >
+      {console.log(testData)}
       <Grid
         item
         xs={12}
@@ -34,6 +51,8 @@ const Register = () => {
         <TextField
           style={{ width: "400px" }}
           placeholder="Full Name"
+          value={fullName}
+          onChange={(e) => setFullName(e.target.value)}
           color="warning"
           InputProps={{
             startAdornment: (
@@ -50,6 +69,8 @@ const Register = () => {
           style={{ width: "400px" }}
           placeholder="Email"
           color="warning"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
@@ -65,6 +86,8 @@ const Register = () => {
           style={{ width: "400px" }}
           placeholder="Nickname"
           color="warning"
+          value={nickName}
+          onChange={(e) => setNickName(e.target.value)}
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
@@ -81,6 +104,8 @@ const Register = () => {
           placeholder="Password"
           type="password"
           color="warning"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
@@ -97,6 +122,8 @@ const Register = () => {
           placeholder="Birthday"
           type={"datetime-local"}
           color="warning"
+          value={birthday}
+          onChange={(e) => setBirthday(e.target.value)}
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
