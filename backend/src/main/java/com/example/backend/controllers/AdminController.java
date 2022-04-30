6ac67f93baed6b9db585af486b981a8cd8fb9cbb
furkanturunc <63520230+furkanturunc@users.noreply.github.com>
2022-04-30 13:33:58 +0300

    getAdminByEmail added in admin controller

--- backend/src/main/java/com/example/backend/controllers/AdminController.java
+++ backend/src/main/java/com/example/backend/controllers/AdminController.java
@@ -3,6 +3,7 @@ package com.example.backend.controllers;
 import com.example.backend.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import com.example.backend.entities.Admin;
 
 @RestController
 @RequestMapping(path = "api/admin")
@@ -18,5 +19,10 @@ public class AdminController {
     @PutMapping("/approve/company/{admin_id}/{company_id}")
     public void approveCompany(@PathVariable String admin_id, @PathVariable String company_id) {
         adminService.approveCompany(admin_id, company_id);
+    }
+    
+    @GetMapping("/{email}")
+    public Admin getAdminByEmail(@PathVariable String email) {
+        return adminService.getAdminByEmail(email);
     }
 }
