92cba56b67b016481338e1fbb49fed99135de703
Arda Serim <arda.serim@ug.bilkent.edu.tr>
2022-05-17 19:44:45 +0300

    updates

--- backend/src/main/java/com/example/backend/repositories/InterviewRepository.java
+++ backend/src/main/java/com/example/backend/repositories/InterviewRepository.java
@@ -181,9 +181,10 @@ public class InterviewRepository {
    }
 
    public List<UserDto> seeResults(String interview_id) {
-      String sql = "SELECT full_name, email, , photo, phone, birth_date, nickname, SUM(question_point) points FROM (users u JOIN people p ON u.user_id = p.person_id) NATURAL JOIN user_interview NATURAL JOIN attempts NATURAL JOIN questions NATURAL JOIN interview_question WHERE is_solved = b'1' AND interview_id = ? GROUP BY nickname ORDER BY points DESC";
+      String sql = "SELECT person_id ,full_name, email, photo, phone, birth_date, nickname, SUM(question_point) points FROM (users u JOIN people p ON u.user_id = p.person_id) NATURAL JOIN user_interview NATURAL JOIN attempts NATURAL JOIN questions NATURAL JOIN interview_question WHERE is_solved = b'1' AND interview_id = ? GROUP BY nickname ORDER BY points DESC";
       return jdbcTemplate.query(sql, (rs, rowNum) -> {
          UserDto userDto = new UserDto();
+         userDto.setPerson_id(rs.getString("person_id"));
          userDto.setFull_name(rs.getString("full_name"));
          userDto.setEmail(rs.getString("email"));
          userDto.setPhone(rs.getString("phone"));
@@ -197,9 +198,10 @@ public class InterviewRepository {
    }
 
    public List<UserDto> seePastResults(String interview_id, String user_id) {
-      String sql = "SELECT full_name, email, , photo, phone, birth_date, nickname, SUM(question_point) points FROM (users u JOIN people p ON u.user_id = p.person_id) NATURAL JOIN user_interview NATURAL JOIN attempts NATURAL JOIN questions NATURAL JOIN interview_question WHERE is_solved = b'1' AND interview_id = ? AND user_id = ? GROUP BY nickname ORDER BY points DESC";
+      String sql = "SELECT person_id, full_name, email, photo, phone, birth_date, nickname, SUM(question_point) points FROM (users u JOIN people p ON u.user_id = p.person_id) NATURAL JOIN user_interview NATURAL JOIN attempts NATURAL JOIN questions NATURAL JOIN interview_question WHERE is_solved = b'1' AND interview_id = ? AND user_id = ? GROUP BY nickname ORDER BY points DESC";
       return jdbcTemplate.query(sql, (rs, rowNum) -> {
          UserDto userDto = new UserDto();
+         userDto.setPerson_id(rs.getString("person_id"));
          userDto.setFull_name(rs.getString("full_name"));
          userDto.setEmail(rs.getString("email"));
          userDto.setPhone(rs.getString("phone"));
