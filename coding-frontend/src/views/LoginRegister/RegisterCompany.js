211050f1e54b43709d557f2ac032339dbed6738c
abdullah-koc <abdullah.koc@ug.bilkent.edu.tr>
2022-05-17 23:14:53 +0300

    Update RegisterCompany.js

--- coding-frontend/src/views/LoginRegister/RegisterCompany.js
+++ coding-frontend/src/views/LoginRegister/RegisterCompany.js
@@ -16,7 +16,8 @@ const RegisterCompany = () => {
   const [companyPassword, setCompanyPassword] = useState("");
 
   const isPasswordValid = (password) => {
-    const regex = /^(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+    const regex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     return regex.test(password);
   };
 
